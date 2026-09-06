ecb953f9e7f24a4bd8098b011362ee43f1daed07
Harshdev06 <engharsh01@gmail.com>
2026-09-06 21:15:38 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>DSA Wale Bhaiya</h1>
+      <p>Ask your doubt. Get the solution.</p>
+
+      <button>Ask a Doubt</button>
+    </div>
+  );
+}
+
+export default App;
